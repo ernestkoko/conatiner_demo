e0cfb433814bb46cd7381afd789d266c1aa35fda
ernestkoko <ernesteferetin@yahoo.com>
2020-04-29 23:15:50 +0100

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,12 +12,29 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text("Container"),
+        ),
+        body: Container(
+          width: 200.0,
+          height: 200.0,
+          margin: EdgeInsets.all(100.0),
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            shape: BoxShape.rectangle,
+            gradient: LinearGradient(
+              begin: Alignment(0.0, -1.0),
+              end: Alignment(0.0, -0.8),
+             colors: [Colors.orange, Colors.deepPurple],
 
+             // colors: [Colors.purple[50], Colors.purple[500], ]
+            ),
+            image: DecorationImage(
+              image: NetworkImage("http://bit.ly/flutter_tiger"),
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(20.0)
+            ),),
+          ),
         ),
-        body: Center(
-          child: Text("Hello world, of course"),
-        ),
-      ),
-    );
+      );
+
   }
 }
